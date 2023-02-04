641ac9e7adee5159431eed7b0a7980806b291476
DevNhaX <nhanguyen7901@gmail.com>
2023-02-04 10:09:09 +0700

    refactor: rename ShoppingCart -> ShoppingItem

---
+++ Mubasa.Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mubasa.Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingItem> ShoppingCarts { get; set; }
+        public double GrandTotal { get; set; } = 0;
+    }
+}
