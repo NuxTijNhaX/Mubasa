87fd726e499df4899e5b6dc86b5d6eab2d641ba8
DevNhaX <nhanguyen7901@gmail.com>
2023-02-08 17:02:28 +0700

    feat: add third parties model(ghn, momo, stripe )

---
+++ Mubasa.Utility/ThirdParties/PaymentGateway/MoMo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mubasa.Utility.ThirdParties.PaymentGateway
+{
+    public class MoMo
+    {
+    }
+}
