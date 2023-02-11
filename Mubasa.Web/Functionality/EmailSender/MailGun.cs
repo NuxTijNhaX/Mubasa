d9ee03ac380d4676faa7585a6de16d8a5e50f30c
DevNhaX <nhanguyen7901@gmail.com>
2023-02-12 00:09:22 +0700

    feat: add mailgun config

---
+++ Mubasa.Web/Functionality/EmailSender/MailGun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mubasa.Web.Functionality.EmailSender
+{
+    public class MailGun
+    {
+        public string Smtp { get; set; }
+        public int Port { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+    }
+}
