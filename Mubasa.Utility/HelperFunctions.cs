c62973c15330bb07e499f8770bd632bd82b71e42
DevNhaX <nhanguyen7901@gmail.com>
2023-02-12 00:05:02 +0700

    feat: add momo payment method, delete strip

---
+++ Mubasa.Utility/HelperFunctions.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mubasa.Utility
+{
+    public class HelperFunctions
+    {
+        public static string ComputeHmacSHA256(string key = "", string message = "")
+        {
+            byte[] keyByte = Encoding.UTF8.GetBytes(key);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            byte[] hashMessage = new HMACSHA256(keyByte).ComputeHash(messageBytes);
+
+            return BitConverter.ToString(hashMessage).Replace("-", "").ToLower();
+        }
+
+        public static string EncodeBase64(string value)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(value);
+
+            return Convert.ToBase64String(bytes);
+        }
+
+        private static async Task<T> PostAsync<T>(string uri, FormUrlEncodedContent content)
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.PostAsync(uri, content);
+            var responseString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(responseString);
+        }
+
+        private static Task<T> PostFormAsync<T>(string uri, Dictionary<string, string> data)
+        {
+            return PostAsync<T>(uri, new FormUrlEncodedContent(data));
+        }
+
+        public static Task<Dictionary<string, string>> PostFormAsync(string uri, Dictionary<string, string> data)
+        {
+            return PostFormAsync<Dictionary<string, string>>(uri, data);
+        }
+    }
+}
--- Mubasa.Utility/SD.cs
+++ Mubasa.Utility/SD.cs
@@ -9,20 +9,28 @@ namespace Mubasa.Utility
     public static class SD
     {
         public const string Role_Individual_User = "Individual";
-        public const string Role_Company_User = "Company";
+        // public const string Role_Company_User = "Company";
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
 
-        public const string OrderStatusPending = "Pending";
-        public const string OrderStatusApproved = "Approved";
-        public const string OrderStatusProcessing = "Processing";
-        public const string OrderStatusShipped = "Shipped";
-        public const string OrderStatusCancelled = "Cancelled";
-        public const string OrderStatusRefunded = "Refunded";
+        public const string PayMethod_COD = "COD";
+        public const string PayMethod_Zalo = "Zalo";
+        public const string PayMethod_MoMo = "MoMo";
 
-        public const string PaymentStatusPending = "Pending";
-        public const string PaymentStatusApproved = "Approved";
-        public const string OrderStatusDelayed = "ApprovedForDelay";
-        public const string PaymentStatusRejected = "Rejected";
+        public const string OrderWait4Pay = "Wait4Paying";
+        public const string OrderPending = "Pending";
+        public const string OrderProcessing = "Processing";
+        public const string OrderShipping= "Shipping";
+        public const string OrderCompleted = "Completed";
+        public const string OrderReturned = "Returned";
+        public const string OrderCancelled = "Cancelled";
+        public const string OrderAll = "All";
+
+        public const string PaymentWaiting = "Waiting";
+        public const string PaymentPaid = "Paid";
+        public const string PaymentRefunded = "Refunded";
+        public const string PaymentCancelled = "Cancelled";
+
+        public const string SessionCart = "CartCount";
     }
 }
