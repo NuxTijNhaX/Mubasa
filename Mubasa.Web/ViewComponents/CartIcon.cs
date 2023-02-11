55643d60ea2d101a39306cb4f34b583a8060e9fc
DevNhaX <nhanguyen7901@gmail.com>
2023-02-12 00:06:45 +0700

    feat: add cart icon component

---
+++ Mubasa.Web/ViewComponents/CartIcon.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mubasa.DataAccess.Repository.IRepository;
+using Mubasa.Utility;
+using System.Security.Claims;
+
+namespace Mubasa.Web.ViewComponents
+{
+    public class CartIcon : ViewComponent
+    {
+        private readonly IUnitOfWork _db;
+        public CartIcon(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                if(HttpContext.Session.GetInt32(SD.SessionCart) == null)
+                {
+                    HttpContext.Session.SetInt32(
+                        SD.SessionCart,
+                        _db.ShoppingItem
+                            .GetAll(i => i.ApplicationUserId == claim.Value)
+                        .ToList().Count);
+                }
+
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+    }
+}
